ce98f8403594d95ee789e2028a123da58e8abfa5
ForteScarlet <ForteScarlet@163.com>
2020-03-28 00:53:05 +0800

    :tada: Initial commit.

---
+++ src/main/kotlin/com/simplekt/RunApplication.kt
@@ -0,0 +1,14 @@
+package com.simplekt
+
+import com.forte.component.forcoolqhttpapi.CoolQHttpApplication
+import com.forte.qqrobot.SimpleRobotApplication
+
+
+fun main() {
+    var application = CoolQHttpApplication()
+}
+
+@SimpleRobotApplication
+class RunApplication {
+
+}
---
+++ src/main/kotlin/com/simplekt/listeners/TestListener1.kt
@@ -0,0 +1,10 @@
+package com.simplekt.listeners
+
+import com.forte.qqrobot.anno.Listen
+import com.forte.qqrobot.beans.messages.types.MsgGetTypes
+
+
+@Listen(MsgGetTypes.privateMsg)
+fun ls(){
+
+}
